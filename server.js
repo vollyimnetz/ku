20356a82c36c5f0315da2fc8e58bd25eae03bc15
Volkmar Kantor <volkmar.kantor@gmx.de>
2024-06-30 10:17:50 +0200

    ipv4 addresses

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import jsonServer from 'json-server';
+import os from 'os';
 const server = jsonServer.create();
 const db = jsonServer.router('local.db.json');
 const middlewares = jsonServer.defaults();
@@ -12,4 +13,32 @@ server.use(middlewares);
 server.use(db);
 server.listen(80, () => {
     console.info('Server läuft auf Port 80 - öffnen Sie http://localhost im Browser');
-})
+
+    const ipv4Addresses = getIPv4Addresses();
+
+    console.log('\nDas ist warscheinlich die öffentliche IPv4 Addresses:');
+    ipv4Addresses.forEach((ip) => {
+        console.log(`\nhttp://${ip}`);
+    });
+});
+
+
+
+// getIPv4Addresses.js
+
+function getIPv4Addresses() {
+  const interfaces = os.networkInterfaces();
+  const addresses = [];
+  const whiteList = ['192.168.0', '192.168.1', '192.168.178', '192.168.179'];
+
+  for(let iface in interfaces) {
+    for(let alias of interfaces[iface]) {
+      if(alias.family === 'IPv4' && !alias.internal) {
+        if(whiteList.some((ip) => alias.address.startsWith(ip))) {
+            addresses.push(alias.address);
+        }
+      }
+    }
+  }
+  return addresses;
+}
